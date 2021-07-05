5e0f1cb1b535af8c820c8f53a27ea4d844ffe557
Jóhannes G. Halldórsson <johannesgh90@gmail.com>
2021-07-05 16:23:16 +0000

    setup script cleanup

--- vim_setup_jgh.sh
+++ vim_setup_jgh.sh
@@ -19,18 +19,12 @@ git clone https://github.com/michalbachowski/vim-wombat256mod ~/.vim/bundle/vim-
 export TERM=xterm-256color
 
 printf "\nInstalling vim-gitgutter"
-git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 
 printf "\nInstalling vim-airline"
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 printf "\nInstalling vim-airline-themes"
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-printf "\nDownloading powerline fonts"
-git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
-printf "\nInstalling powerline fonts"
-~/fonts/install.sh
-printf "\nCleaning up after fonts install"
-rm -rf ~/fonts
 
 printf "\nInstalling nerdtree"
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
@@ -44,5 +38,12 @@ git clone git://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
 printf "\nInstalling ftplugin"
 mkdir -p ~/.vim/ftplugin
 wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+
+printf "\nDownloading powerline fonts"
+git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
+printf "\nInstalling powerline fonts"
+~/fonts/install.sh
+printf "\nCleaning up after fonts install"
+rm -rf ~/fonts
 
 printf "End of script"
