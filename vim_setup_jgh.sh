aeebe2beb022f59f754c009c5151780ec03ad92c
Jóhannes G. Halldórsson <johannesgh90@gmail.com>
2021-07-06 02:05:10 +0000

    shell script clean up

--- vim_setup_jgh.sh
+++ vim_setup_jgh.sh
@@ -3,8 +3,10 @@
 printf "About to install several vim extentions..."
 printf "\nPrerequisites: vim wget git\n"
 printf "\nYCM Auto-Complete: build-essential cmake python3-dev nodejs npm"
-printf "\nYCM Auto-Complete extra: mono-complete (C#) golang (Go) default-jdk (Java)\n"
-printf "\nPython (3): python3-jedi python3-flake8 flake8 python3-flake8-docstrings"
+printf "\nYCM Auto-Complete extra: mono-complete (C#) golang (Go)"
+printf " default-jdk (Java)\n"
+printf "\nPython (3): python3-jedi python3-flake8 flake8"
+printf " python3-flake8-docstrings"
 printf "\nBash: python3-bashate python-bashate-doc"
 
 printf "\nDo you want to continue (Y/n)?"
@@ -13,32 +15,57 @@ if echo "$user_answer" | grep -iq "^n" ;then
     exit 1
 fi
 
+VIM_DIR="$HOME/.vim"
+
+function pathogen_install {
+        if [[ $# -eq 2 || $# -eq 3 ]]; then
+                local gh_user=$1
+                local gh_repo=$2
+                if [[ $# -eq 3 ]]; then
+                        local n_levels=$3
+                else
+                        local n_levels=""
+                fi
+        else
+                printf "\nThis function accepts 2 or 3 arguments"
+                printf "not $#.\n"
+                exit 2
+        fi
+        local source_url="https://github.com/$gh_user/$gh_repo.git"
+        local dest_dir="$VIM_DIR/bundle/$gh_repo"
+        if [[ -z "$n_levels" ]]; then
+                git clone $source_url $dest_dir
+        else
+                git clone --depth=$n_levels $source_url $dest_dir
+        fi
+}
+
 printf "\nInstalling Pathogen"
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-wget -O ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+mkdir -p $VIM_DIR/autoload $VIM_DIR/bundle
+wget -O $VIM_DIR/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # GUI and Git
 
 printf "\nInstalling Wombat256Mod"
-git clone https://github.com/michalbachowski/vim-wombat256mod ~/.vim/bundle/vim-wombat256mod
+pathogen_install michalbachowski vim-wombat256mod
 export TERM=xterm-256color
 printf "\nInstalling vim-gitgutter"
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+pathogen_install airblade vim-gitgutter
 printf "\nInstalling vim-airline"
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+pathogen_install vim-airline vim-airline
 printf "\nInstalling vim-airline-themes"
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+pathogen_install vim-airline vim-airline-themes
 printf "\nInstalling nerdtree"
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+pathogen_install scrooloose nerdtree
 printf "\nInstalling nerdtree-git-plugin"
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+pathogen_install Xuyuanp nerdtree-git-plugin
 
 # YouCompleteMe Auto-Complete
 
 printf "\nInstalling YouCompleteMe Auto-Complete"
-git clone https://github.com/ycm-core/YouCompleteMe ~/.vim/bundle/YouCompleteMe
+pathogen_install ycm-core YouCompleteMe
 working_dir=`pwd`
-cd ~/.vim/bundle/YouCompleteMe
+cd $VIM_DIR/bundle/YouCompleteMe
 git submodule update --init --recursive
 python3 install.py --clangd-completer --ts-completer --rust-completer
 # Add to above for additional Auto-complete: --cs-completer --go-completer --java-completer
@@ -48,43 +75,37 @@ cd $working_dir
 # Ale Linter
 
 printf "\nInstalling Ale Linter"
-git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
+pathogen_install dense-analysis ale
 # ESLint for JavaScript
 npm -i -g eslint eslint-plugin-vue
 
 # Python
 
-# Python Auto-Complete, replaced by YCM
-# printf "\nInstalling jedi-vim"
-# git clone https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
-
-# Python linter, replaced by Ale
-# printf "\nInstalling vim-flake8"
-# git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
-
 printf "\nInstalling Python PEP-8 line length guide script and folding script."
-mkdir -p ~/.vim/ftplugin
-cp ./python.vim ~/.vim/ftplugin/python.vim
-wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+mkdir -p $VIM_DIR/ftplugin
+cp ./python.vim $VIM_DIR/ftplugin/python.vim
+source_url="http://www.vim.org/scripts/download_script.php?src_id=5492"
+dest_dir="$VIM_DIR/ftplugin/python_editing.vim"
+wget -O $dest_dir $source_url
 
 # Web development
 
 printf "\nInstalling Emmet-vim for HTML and CSS abbreviations"
-git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+pathogen_install mattn emmet-vim
 
 printf "\nInstalling vim-css-color"
-git clone https://github.com/ap/vim-css-color.git ~/.vim/bundle/vim-css-color
+pathogen_install ap vim-css-color
 
 printf "\nInstalling typescript-vim"
-git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
+pathogen_install leafgarland typescript-vim
 
 printf "\nInstalling vim-vue"
-git clone https://github.com/posva/vim-vue.git ~/.vim/bundle/vim-vue
+pathogen_install posva vim-vue
 
 # Rust
 
 printf "\nInstalling rust.vim"
-git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
+pathogen_install rust_lang rust.vim 1
 
 # Font
 
