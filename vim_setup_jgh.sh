30743a3034a16ebb4da6c9a0af05cf35dee9b38f
Jóhannes G. Halldórsson <johannesgh90@gmail.com>
2021-07-06 18:34:10 +0000

    script cleanup and onedark colorscheme added

--- vim_setup_jgh.sh
+++ vim_setup_jgh.sh
@@ -40,48 +40,65 @@ function pathogen_install {
         fi
 }
 
-printf "\nInstalling Pathogen"
+printf "\nInstalling Pathogen\n"
 mkdir -p $VIM_DIR/autoload $VIM_DIR/bundle
 wget -O $VIM_DIR/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # GUI and Git
 
-printf "\nInstalling Wombat256Mod"
+# Back-up color scheme
+printf "\nInstalling Wombat256Mod\n"
 pathogen_install michalbachowski vim-wombat256mod
 export TERM=xterm-256color
-printf "\nInstalling vim-gitgutter"
+
+# Main Color Scheme
+printf "\nInstalling onedark\n"
+pathogen_install joshdick onedark.vim
+
+# Other GUI plugins
+printf "\nInstalling vim-gitgutter\n"
 pathogen_install airblade vim-gitgutter
-printf "\nInstalling vim-airline"
+printf "\nInstalling vim-airline\n"
 pathogen_install vim-airline vim-airline
-printf "\nInstalling vim-airline-themes"
+printf "\nInstalling vim-airline-themes\n"
 pathogen_install vim-airline vim-airline-themes
-printf "\nInstalling nerdtree"
+printf "\nInstalling nerdtree\n"
 pathogen_install scrooloose nerdtree
-printf "\nInstalling nerdtree-git-plugin"
+printf "\nInstalling nerdtree-git-plugin\n"
 pathogen_install Xuyuanp nerdtree-git-plugin
 
 # YouCompleteMe Auto-Complete
 
-printf "\nInstalling YouCompleteMe Auto-Complete"
-pathogen_install ycm-core YouCompleteMe
-working_dir=`pwd`
-cd $VIM_DIR/bundle/YouCompleteMe
-git submodule update --init --recursive
-python3 install.py --clangd-completer --ts-completer --rust-completer
-# Add to above for additional Auto-complete: --cs-completer --go-completer --java-completer
-# Or --all for everything enabled.
-cd $working_dir
+printf "\nInstalling YouCompleteMe Auto-Complete\n"
+
+if [[ -d "$VIM_DIR/bundle/YouCompleteMe" ]]; then
+        ALREADY_INSTALLED=1
+else
+        ALREADY_INSTALLED=0
+fi
+
+if [[ $ALREADY_INSTALLED -eq 0 ]]; then
+        pathogen_install ycm-core YouCompleteMe
+        working_dir=`pwd`
+        cd $VIM_DIR/bundle/YouCompleteMe
+        git submodule update --init --recursive
+        python3 install.py --clangd-completer --ts-completer --rust-completer
+        # Add to above for additional Auto-complete:
+        # --cs-completer --go-completer --java-completer
+        # Or --all for everything enabled.
+        cd $working_dir
+fi
 
 # Ale Linter
 
-printf "\nInstalling Ale Linter"
+printf "\nInstalling Ale Linter\n"
 pathogen_install dense-analysis ale
 # ESLint for JavaScript
 npm -i -g eslint eslint-plugin-vue
 
 # Python
 
-printf "\nInstalling Python PEP-8 line length guide script and folding script."
+printf "\nInstalling Python PEP-8 line length guide script and folding script.\n"
 mkdir -p $VIM_DIR/ftplugin
 cp ./python.vim $VIM_DIR/ftplugin/python.vim
 source_url="http://www.vim.org/scripts/download_script.php?src_id=5492"
@@ -90,30 +107,30 @@ wget -O $dest_dir $source_url
 
 # Web development
 
-printf "\nInstalling Emmet-vim for HTML and CSS abbreviations"
+printf "\nInstalling Emmet-vim for HTML and CSS abbreviations\n"
 pathogen_install mattn emmet-vim
 
-printf "\nInstalling vim-css-color"
+printf "\nInstalling vim-css-color\n"
 pathogen_install ap vim-css-color
 
-printf "\nInstalling typescript-vim"
+printf "\nInstalling typescript-vim\n"
 pathogen_install leafgarland typescript-vim
 
-printf "\nInstalling vim-vue"
+printf "\nInstalling vim-vue\n"
 pathogen_install posva vim-vue
 
 # Rust
 
-printf "\nInstalling rust.vim"
+printf "\nInstalling rust.vim\n"
 pathogen_install rust_lang rust.vim 1
 
 # Font
 
-printf "\nDownloading powerline fonts"
+printf "\nDownloading powerline fonts\n"
 git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
-printf "\nInstalling powerline fonts"
+printf "\nInstalling powerline fonts\n"
 ~/fonts/install.sh
-printf "\nCleaning up after fonts install"
+printf "\nCleaning up after fonts install\n"
 rm -rf ~/fonts
 
-printf "End of script"
+printf "\nEnd of script\n"
